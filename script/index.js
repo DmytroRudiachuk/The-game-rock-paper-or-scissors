6649cbacbd23f7db9b33431339a81529eedb6cd5
keri19372 <keri19372@gmail.com>
2017-03-23 11:56:56 +0200

    Debug

--- script/index.js
+++ script/index.js
@@ -2,7 +2,7 @@ var userChoice = prompt("Do you choose rock, paper or scissors?");
 var computerChoice = Math.random();
 if (computerChoice < 0.34) {
 	computerChoice = "rock";
-} else if(computerChoice <= 0.67) {
+} else if(computerChoice < 0.67) {
 	computerChoice = "paper";
 } else {
 	computerChoice = "scissors";
@@ -10,19 +10,19 @@ if (computerChoice < 0.34) {
 
 var compare = function(choice1, choice2) {
     if(choice1 === choice2) {
-        alert("The result is a tie")
+        alert("The result is a tie! Play Again")
     } 
     
     else if(choice1 === "rock") {        
         if(choice2 === "scissors") {
             alert("rock wins")
         } else {
-           alert("peper wins")
+           alert("paper wins")
             }}
             
             else if(choice1 === "paper"){
                 if(choice2 === "rock") {
-                    alert("peper wins")
+                    alert("paper wins")
                     } else {
                        alert("scissors wins")
                         }}
@@ -31,8 +31,8 @@ var compare = function(choice1, choice2) {
                     if(choice2 === "rock") {
                         alert("rock wins")
                         } else {
-                            alert("peper wins")
+                            alert("scissors wins")
                         }}                    
 };
 
-console.log(compare(computerChoice, userChoice))
+console.log(compare(userChoice, computerChoice))
